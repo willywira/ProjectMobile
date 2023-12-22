44a2cdde9e658326351ed64fd7efd27e7e6560de
willy <willy21si@mahasiswa.pcr.ac.id>
2023-12-22 11:59:56 +0700

    progress project

---
+++ app/src/main/java/com/example/projectmobile/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.projectmobile
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projectmobile.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.btnRegister.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
